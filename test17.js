4d0babc08feb8adcf3f8d2da2fa756dc02aaedd6
Rodrigo Botti <rodrigo.botti@gmail.com>
2018-05-03 12:09:54 -0300

    Improvement: curry without bind

--- test17.js
+++ test17.js
@@ -2,5 +2,5 @@ module.exports = function curry (fn, n) {
   const arity = n || fn.length
   return (...params) => params.length >= arity
     ? fn(...params)
-    : curry(fn.bind(fn, ...params), arity - params.length)
+    : curry((...rest) => fn(...params, ...rest), arity - params.length)
 }
